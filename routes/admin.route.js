09bab1df0d96b2149085210936f9cb8cbef36601
Shashwat Mahendra <shashwatmahender2104@gmail.com>
2023-07-13 19:36:23 +0530

    crud done

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const adminrouter = express.Router();
-
+const axios = require("axios");
 const { adminmodel } = require("../models/admin.model");
+require("dotenv").config();
 
 adminrouter.post("/questions", async (req, res) => {
   try {
@@ -58,5 +59,106 @@ adminrouter.post("/questions/:id/testcases", async (req, res) => {
     console.log(error);
   }
 });
+
+// adminrouter.post("/questions/:id/submit", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const { code, language } = req.body;
+
+//     const question = await adminmodel.findById(id);
+//     if (!question) {
+//       return res.status(404).json({ message: "Question not found" });
+//     }
+
+//     // Find test cases for question
+//     const testCases = question.testcases;
+
+//     const results = [];
+//     for (const testCase of testCases) {
+//       const response = await axios.post(
+//         // console.log("j")
+//         "https://d6ee0a57.compilers.sphere-engine.com/api/v4/submissions?access_token=e2c1dc11d7c91025751647382b3bc543",
+//         {
+//           language,
+//           sourceCode: code,
+//           input: testCase.input,
+//         }
+//       );
+//       console.log(response.sourceCode);
+//       const submissionId = response.data.id;
+//       console.log(submissionId, "heloo");
+//       let status = "running";
+//       while (status === "running") {
+//         const statusResponse = await axios.get(
+//           `https://d6ee0a57.compilers.sphere-engine.com/api/v4/submissions/${submissionId}?access_token=e2c1dc11d7c91025751647382b3bc543`
+//         );
+//         status = statusResponse.data.status;
+//       }
+//       if (status === "accepted" && response.data.output === testCase.output) {
+//         results.push("success");
+//       } else {
+//         results.push("wrong");
+//       }
+//     }
+
+//     if (results.every((result) => result === "success")) {
+//       res.status(200).json({ message: "Solution is correct" });
+//     } else if (results.some((result) => result === "success")) {
+//       res.status(200).json({ message: "Some test cases passed, some failed" });
+//     } else {
+//       res.status(200).json({ message: "Solution is wrong" });
+//     }
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+// });
+
+
+
+adminrouter.post("/submit-solution", async (req, res) => {
+  try {
+    const { question_id, answer } = req.body;
+
+    const question = await adminmodel.findById(question_id);
+
+    const data = {
+      question_id: question.sphere_engine_question_id,
+      answer,
+    };
+
+    const headers = { Authorization: `Bearer ${process.env.SPHERE_ENGINE_API_KEY}` };
+    const response = await axios.post(
+      `${process.env.SPHERE_ENGINE_API_URL}/check-solution`,
+      data,
+      { headers }
+    );
+
+    if (response.status === 200) {
+      const result = response.data;
+      if (result.correct) {
+        res.json({ result: "success" });
+      } else {
+        const error_message = result.error_message;
+        res.json({ result: "wrong", error_message });
+      }
+    } else {
+      res.status(500).json({
+        result: "error",
+        error_message: "An error occurred while checking the solution",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ result: "error", error_message: "Internal server error" });
+  }
+});
+
+
+
+
+
+
 
 module.exports = { adminrouter };
